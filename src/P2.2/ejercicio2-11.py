b672871c8a5411dfaeffe515b7c9d96c915d1754
ivanjim13 <ivangorrion57@gmail.com>
2023-10-30 14:42:44 +0100

    P2.2 desde casa

---
+++ src/P2.2/ejercicio2-11.py
@@ -0,0 +1,12 @@
+# Escribir un programa que pida al usuario una palabra y luego muestre por pantalla una a una las letras de la palabra introducida empezando por la última.
+
+def deletrear(palabra):
+    cont = palabra.len()
+    letra = palabra[-1]
+    while cont > 0:
+        cont = cont - 1
+        print(letra)
+
+palabra = input("Introduce una palabra")
+
+deletrear(palabra)
