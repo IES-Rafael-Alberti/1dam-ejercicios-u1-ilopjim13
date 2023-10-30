1f00bf2939689e4b97030e1679b6a25cc16be082
ilopjim13 <ljivan@iesrafaelalberti.es>
2023-10-30 20:28:38 +0100

    desde clase

--- src/P2.2/ejercicio2-1.py
+++ src/P2.2/ejercicio2-1.py
@@ -1,11 +1,13 @@
 # Escribir un programa que pida al usuario una palabra y la muestre por pantalla 10 veces.
+def main():
+    palabra = input("Introduce una palabra: ")
+
 
 def palabras(palabra):
     count = 1
     while count <= 10:
         count += 1
-        print("palabra")
+        print(palabra)
 
-palabra = input("Introduce una palabra: ")
 
-palabras(palabra)
+palabras()
